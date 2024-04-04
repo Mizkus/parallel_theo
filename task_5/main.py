e7e80e22a6fca339b838e93c22b5932c29a6a3fc
mizkus <kirill131@gmail.com>
2024-04-04 23:42:41 +0700

    main

--- task_5/main.py
+++ task_5/main.py
@@ -27,54 +27,55 @@ class parallel_model:
                 break
             if size == None: size = frame.shape
 
-            q[c % num_threads].put(frame)
+            q[c % num_threads].put((frame, c))
             c += 1
         return c, size
             
     def do_res(self, model, q, out_q):
         while True:
             try:
-                frame = q.get(timeout=1)
+                frame, ind = q.get(timeout=1)
                 result = model(frame)
-                out_q.put(result[0].plot())
+                out_q.put((result[0].plot(), ind))
             except:
                 break
             
     def get_pose_video(self):
         
         queues = np.array([Queue() for _ in range(self.num_threads)])
-        out_queues = np.array([Queue() for _ in range(self.num_threads)])
+        out_q = Queue()
         
         len, size = self.read_frames(queues, self.num_threads)
         threads = list()
         for i in range(self.num_threads):
 
             model = YOLO(self.model)
-            thread = threading.Thread(target=self.do_res, args=(model, queues[i], out_queues[i]))
+            thread = threading.Thread(target=self.do_res, args=(model, queues[i], out_q))
             threads.append(thread)
+            print(i)
     
         for thread in threads:
             thread.start()
 
         start_time = time.time()
-        c = 0
+        c = 1
         
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video_writer = cv2.VideoWriter('video_out.mp4', fourcc, 30, (size[1], size[0]))
-        
+        frames = [None] * len
         while True:
             try:
-                res = out_queues[c % self.num_threads].get(timeout=10)
-                video_writer.write(res)
+                frame, ind = out_q.get(timeout=10)
+                frames[ind] = frame
                 print(f'frame {c} of {len}')
                 c += 1
             except:
                 break
-            
-
-
+        
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        video_writer = cv2.VideoWriter('video_out.mp4', fourcc, 30, (size[1], size[0]))
+        for frame in frames:
+            video_writer.write(frame)
         video_writer.release()
         print("TIME: ", time.time() - start_time)
 
-a = parallel_model(6, 'video.mp4')
+a = parallel_model(1, 'video.mp4')
 a.get_pose_video()
